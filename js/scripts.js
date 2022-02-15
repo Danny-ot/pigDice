a959793a394eebc5f7cfe64e885eb195c09932bd
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-02-15 00:56:31 -0800

    Create the method for adding player1 score

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 
 // Constructor For The PigDice
-function pigDice(){
+function PigDice(){
  this.player1 = 0
  this.player2 = 0
 }
+
+//  Method For Adding Player1 Score
+PigDice.prototype.addPlayer1Score = function(score){
+    this.player1 += score;
+}
+
+// Method For Adding 
